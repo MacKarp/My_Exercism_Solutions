9f61e09fc6596a031dfca5f0c5b7aac18b69079e
Karpfly <karpfly@gmail.com>
2020-12-31 20:27:14 +0100

    Added Rust Difference Of Squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    let mut sum: u32 = 0;
+    for i in 0..=n {
+        sum += i;
+    }
+    sum.pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    let mut sum = 0;
+    for i in 0..=n {
+        sum += i.pow(2);
+    }
+    sum
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}

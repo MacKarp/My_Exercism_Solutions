7ffad90496dbe9bc381db02b7122ee6e17908a35
Karpfly <karpfly@gmail.com>
2021-01-13 23:40:20 +0100

    Added Rust High Scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut output = self.scores.to_vec();
+        output.sort();
+        output.reverse();
+        output.iter().take(3).copied().collect()
+    }
+}
